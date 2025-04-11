cc5119f315ffbf39d1c5b636322bb79a0369e580
yemingrujing <yemingrujing@gmail.com>
2025-04-11 18:07:20 +0800

    测试

---
+++ mysql_mcp.py
@@ -0,0 +1,27 @@
+from mcp.server.fastmcp import FastMCP
+import pymysql
+
+mcp = FastMCP("mysql_mcp")
+
+@mcp.tool()
+def analysis_data(amt: int) -> int:
+    conn = pymysql.connect(
+        host="192.168.182.109",
+        port=3306,
+        user="root",
+        password="1234567890",
+        database="yifei_dev"
+    )
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT COUNT(*) FROM `tmall_天猫_推广发票_总览` where `开票金额（元）` > {amt}")
+    result = cursor.fetchone()[0]
+    cursor.close()
+    conn.close()
+    return result
+
+if __name__ == "__main__":
+    try:
+        mcp.run(transport='stdio')
+    except Exception as e:
+        logger.error(f"服务器运行失败: {str(e)}")
+        sys.exit(1)
