1c3a4ffbe288e12cbd5bef88ac8dd692761d8a1c
guang <guangwei@zmhui.com>
2025-04-12 20:43:44 +0800

    优化

--- mysql_server.py
+++ mysql_server.py
@@ -17,7 +17,7 @@ from mcp.server.fastmcp import FastMCP
 
 # 配置日志
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.ERROR,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[logging.StreamHandler(sys.stderr)]
 )
